e0884f218b2a2f0420573cefb9e21ca299254185
Def <codef.me@gmail.com>
2015-08-06 15:18:13 +0700

    Update MySqlDB.cs

--- MySqlDB.cs
+++ MySqlDB.cs
@@ -148,5 +148,78 @@ namespace DB
                 cmd = null;
             }
         }
+        public void Backup()
+        {
+            try
+               {
+                 DateTime Time = DateTime.Now;
+                 int year = Time.Year;
+                 int month = Time.Month;
+                 int day = Time.Day;
+                 int hour = Time.Hour;
+                 int minute = Time.Minute;
+                 int second = Time.Second;
+                 int millisecond = Time.Millisecond;
+         
+                 //Save file to C:\ with the current date as a filename
+                 string path;
+                 path = "C:\\MySqlBackup" + year + "-" + month + "-" + day + 
+         	"-" + hour + "-" + minute + "-" + second + "-" + millisecond + ".sql";
+                 StreamWriter file = new StreamWriter(path);
+         
+                 
+                 ProcessStartInfo psi = new ProcessStartInfo();
+                 psi.FileName = "mysqldump";
+                 psi.RedirectStandardInput = false;
+                 psi.RedirectStandardOutput = true;
+                 psi.Arguments = string.Format(@"-u{0} -p{1} -h{2} {3}", 
+         			uid, password, server, database);
+                 psi.UseShellExecute = false;
+         
+                 Process process = Process.Start(psi);
+         
+                 string output;
+                 output = process.StandardOutput.ReadToEnd();
+                 file.WriteLine(output);
+                 process.WaitForExit();
+                 file.Close();
+                 process.Close();
+             }
+             catch (IOException ex)
+             {
+                 MessageBox.Show("Error , unable to backup!");
+             }
+         }
+         public void Restore()
+         {
+             try
+             {
+                 //Read file from C:\
+                 string path;
+                 path = "C:\\MySqlBackup.sql";
+                 StreamReader file = new StreamReader(path);
+                 string input = file.ReadToEnd();
+                 file.Close();
+         
+                 ProcessStartInfo psi = new ProcessStartInfo();
+                 psi.FileName = "mysql";
+                 psi.RedirectStandardInput = true;
+                 psi.RedirectStandardOutput = false;
+                 psi.Arguments = string.Format(@"-u{0} -p{1} -h{2} {3}", 
+         			uid, password, server, database);
+                 psi.UseShellExecute = false;
+         
+                 
+                 Process process = Process.Start(psi);
+                 process.StandardInput.WriteLine(input);
+                 process.StandardInput.Close();
+                 process.WaitForExit();
+                 process.Close();
+             }
+             catch (IOException ex)
+             {
+                 MessageBox.Show("Error , unable to Restore!");
+             }
+         }
     }
 }
